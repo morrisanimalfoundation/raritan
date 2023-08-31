68470dc31cfd23f9381ce97142575486cb4670d6
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2023-08-31 11:21:33 -0600

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='raritan',
+   version='1.0',
+   description='A ',
+   author='Morris Animal Foundation',
+   author_email='information-systems@morrisanimalfoundation.org',
+   packages=['raritan'],
+   install_requires=['rich', 'isort', 'flake8'],
+)
