3b2ca5faabcaefcdd24b3564d289b05d4c2a2f0a
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2023-09-12 14:14:32 -0600

    Try and create a settings.py.

--- sort_and_lint.sh
+++ sort_and_lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Perform a little cleaning on our imports.
 echo "Using isort utility..."
-isort --diff .
+isort .
 # Lint our python scripts ignoring the contested line length (PEP8 E501).
 echo "Linting with flake8..."
 flake8 --ignore=E501 --exclude=__pycache__,sort_and_lint.sh,data,.git,.gitignore,Dockerfile,README.md,.idea,*.feature,*.txt,*.json,*.sh raritan/*.py
